3d5d9ed5c45062c16f6dcc723eb636e5179e6ebc
nanogens <37455949+nanogens@users.noreply.github.com>
2019-02-27 19:08:11 -0500

    Updating
    Nothing more to say..

--- ZynqTraining/Session_07_V_Lab_01_SimulationForAXIStreamModule/axis-example/vivado/project_2/project_2.srcs/sim_1/new/sample_generator_testbench.vhd
+++ ZynqTraining/Session_07_V_Lab_01_SimulationForAXIStreamModule/axis-example/vivado/project_2/project_2.srcs/sim_1/new/sample_generator_testbench.vhd
@@ -34,59 +34,64 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity sample_generator_testbench is
 --  Port ( );
   port (
-    axi_en : in STD_LOGIC = '0';
-    en : in STD_LOGIC;
-    framesize : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    axi_en : out STD_LOGIC;
+    en : out STD_LOGIC;
+    framesize : out STD_LOGIC_VECTOR ( 7 downto 0 );
 	
-    M_AXIS_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-    M_AXIS_tlast : out STD_LOGIC;
-    M_AXIS_tready : in STD_LOGIC;
-    M_AXIS_tstrb : out STD_LOGIC_VECTOR ( 3 downto 0 );
-    M_AXIS_tvalid : out STD_LOGIC;
+    M_AXIS_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    M_AXIS_tlast : in STD_LOGIC;
+    M_AXIS_tready : out STD_LOGIC; -- "a signal that we drive and should be a register that we drive"
+    M_AXIS_tstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    M_AXIS_tvalid : in STD_LOGIC;
 	
-    S_AXIS_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-    S_AXIS_tlast : in STD_LOGIC;
-    S_AXIS_tready : out STD_LOGIC;
-    S_AXIS_tstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
-    S_AXIS_tvalid : in STD_LOGIC;
+    S_AXIS_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    S_AXIS_tlast : out STD_LOGIC;
+    S_AXIS_tready : in STD_LOGIC;
+    S_AXIS_tstrb : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    S_AXIS_tvalid : out STD_LOGIC;
 
-    m_axis_aclk : in STD_LOGIC;
-    s_axis_aresetn : in STD_LOGIC;
+    aclk : out STD_LOGIC;
+    aresetn : out STD_LOGIC;
   );
-end sample_generator_testbench;
+end entity sample_generator_testbench;
 
 architecture Behavioral of sample_generator_testbench is
     signal axi_en_s : STD_LOGIC:='0';
     signal en_s : STD_LOGIC:='0';
     signal framesize_s : STD_LOGIC_VECTOR ( 7 downto 0 );
 	--master port
-    signal M_AXIS_tdata_s : STD_LOGIC_VECTOR ( 31 downto 0 ) = (others => '0');
+    signal M_AXIS_tdata_s : STD_LOGIC_VECTOR ( 31 downto 0 ) := (others => '0');
     signal M_AXIS_tlast_s : STD_LOGIC:='0';
-    --M_AXIS_tready_s : STD_LOGIC = '0';
-    signal M_AXIS_tstrb_s : STD_LOGIC_VECTOR ( 3 downto 0 ) = (others => '0');
+    signal M_AXIS_tready_s : STD_LOGIC:= '0';
+    signal M_AXIS_tstrb_s : STD_LOGIC_VECTOR ( 3 downto 0 ) := (others => '0');
     signal M_AXIS_tvalid_s : STD_LOGIC:='0';
 	--slave port
-    signal S_AXIS_tdata_s : STD_LOGIC_VECTOR ( 31 downto 0 ) = (others => '0');
+    signal S_AXIS_tdata_s : STD_LOGIC_VECTOR ( 31 downto 0 ) := (others => '0');
     signal S_AXIS_tlast_s : STD_LOGIC:='0';
     --S_AXIS_tready_s : STD_LOGIC;
-    signal S_AXIS_tstrb_s : STD_LOGIC_VECTOR ( 3 downto 0 ) = (others => '0');
+    signal S_AXIS_tstrb_s : STD_LOGIC_VECTOR ( 3 downto 0 ) := (others => '0');
     signal S_AXIS_tvalid_s : STD_LOGIC:='0';
   
-    signal m_axis_aclk_s : STD_LOGIC:='0';
-    signal s_axis_aresetn_s : STD_LOGIC:='1';	
+    signal aclk_s : STD_LOGIC:='0';
+    signal aresetn_s : STD_LOGIC:='1';	
 	
 	signal start_s : STD_LOGIC:='0';
 	signal start_delaycount_s : INTEGER:=0;	
 	signal start_delayflag_s : STD_LOGIC:='0';
+
+    signal aclk_scale_s : INTEGER:=0;
+    signal aclkedge_s2 : STD_LOGIC:='0';
+
 begin
- 
-  if s_axis_aresetn_s = '1' then -- start up state of reset if high
-    start_delaycount_s <= '0';
+ FN : process(aclkedge_s2, aresetn_s) 
+ begin
+   if aresetn_s = '1' then -- start up state of reset if high
+    start_delaycount_s <= 0;
 	start_delayflag_s <= '0';
   end if;
   
-  if m_axis_aclk_s'event and m_axis_aclk_s = '1' and start_delayflag_s = '0' then
-	if(start_delaycount_s = 255) then	
+  if aclkedge_s2'event and aclkedge_s2 = '1' and start_delayflag_s = '0' then
+	if start_delaycount_s = 255 then	
 	    start_delayflag_s <= '1';
 	else
         start_delaycount_s <= start_delaycount_s + 1;	
@@ -97,41 +102,57 @@ begin
   if start_delayflag_s = '1' then
 
     axi_en_s <= '0';
-    framesize_s <= '0';
+    framesize_s <= x"10";
   
-    S_AXIS_tdata_s <= '0';
+    S_AXIS_tdata_s <= (others => '0');
     S_AXIS_tlast_s <= '0';
-    S_AXIS_tstrb_s <= '0';
+    S_AXIS_tstrb_s <= (others => '0');
     S_AXIS_tvalid_s <= '0';
   end if;    
-  
+
+end process FN;
 
 
-end Behavioral;
+CLK : process(aclk_s) 
+begin
+    if aclk_s = '1' then
+      if aclk_scale_s > 250 then
+	    aclk_scale_s <= 0;
+	    aclkedge_s2 <= '1';
+	  else 
+	    aclk_scale_s <= aclk_scale_s + 1;
+	    aclkedge_s2 <= '0';
+	  end if;
+	end if;
+end process CLK;
 
-s_axis_aresetn_s <= s_axis_aresetn;
 
-m_axis_aclk_s <= '1' when m_axis_aclk = '1' and s_axis_aresetn_s = '0' else '0';
+aresetn <= aresetn_s;
+
+aclk_s <= '1' when aclk_s = '1' and aresetn_s = '0' else '0';
+
+
 	
 axi_en <= axi_en_s;
 en <= en_s;
-framesize <= framesize_s;
+framesize ( 7 downto 0 ) <= framesize_s ( 7 downto 0 );
 
 --master port
-M_AXIS_tdata <= M_AXIS_tdata_s;
-M_AXIS_tlast <= M_AXIS_tlast_s;
+M_AXIS_tdata_s <= M_AXIS_tdata;
+M_AXIS_tlast_s <= M_AXIS_tlast;
 M_AXIS_tready <= M_AXIS_tready_s;
-M_AXIS_tstrb (3 downto 0) <= M_AXIS_tdata_s ( 3 downto 0 );
-M_AXIS_tvalid <= M_AXIS_tvalid_s;
+M_AXIS_tstrb_s (3 downto 0) <= M_AXIS_tdata ( 3 downto 0 );
+M_AXIS_tvalid_s <= M_AXIS_tvalid;
 
 --slave port
 S_AXIS_tdata (31 downto 0) <= S_AXIS_tdata_s ( 31 downto 0 );
 S_AXIS_tlast <= S_AXIS_tlast_s;
-S_AXIS_tready <= S_AXIS_tready_s;
-S_AXIS_tstrb ( 3 downto 0 ) <= S_AXIS_tstrb_s;
+--S_AXIS_tready_s <= S_AXIS_tready;
+S_AXIS_tstrb ( 3 downto 0 ) <= S_AXIS_tstrb_s ( 3 downto 0 );
 S_AXIS_tvalid <= S_AXIS_tvalid_s;
   
 --misc port
-m_axis_aclk <= m_axis_aclk_s;
-s_axis_aresetn <= s_axis_aresetn_s;
+aclk <= aclk_s;
+aresetn <= aresetn_s;
 
+end Behavioral;
