174be4c9d1b04f49ff49ecf285f319d1005182b2
nanogens <37455949+nanogens@users.noreply.github.com>
2019-03-17 19:37:15 -0400

    Updating
    Nothing more to say..

--- ZynqTraining/Session_07_V_Lab_01_SimulationForAXIStreamModule/axis-example/vivado/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/sample_generator_testbench_behav/obj/xsim_1.c
+++ ZynqTraining/Session_07_V_Lab_01_SimulationForAXIStreamModule/axis-example/vivado/project_2/project_2.sim/sim_1/behav/xsim/xsim.dir/sample_generator_testbench_behav/obj/xsim_1.c
@@ -90,8 +90,8 @@ const int NumRelocateId= 40;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/sample_generator_testbench_behav/xsim.reloc",  (void **)funcTab, 40);
-	iki_vhdl_file_variable_register(dp + 22352);
-	iki_vhdl_file_variable_register(dp + 22408);
+	iki_vhdl_file_variable_register(dp + 22272);
+	iki_vhdl_file_variable_register(dp + 22328);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
